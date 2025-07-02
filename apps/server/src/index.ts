468dcbfdc07353141f89f57a0dc2590b4d40b315
sanskarprasad <psanskarkumar01@gmail.com>
2025-07-02 13:12:01 +0530

    feat:initial server added

---
+++ apps/server/src/index.ts
@@ -0,0 +1,18 @@
+import http from "http";
+
+async function init() {
+
+
+  const httpServer = http.createServer();
+  const PORT = process.env.PORT ? process.env.PORT : 8000;
+
+
+
+  httpServer.listen(PORT, () =>
+    console.log(`HTTP Server started at PORT:${PORT}`)
+  );
+
+
+}
+
+init();
